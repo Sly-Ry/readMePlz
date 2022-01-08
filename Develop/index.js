e4d18849cfad65471712141211e60781a35e236d
ryan <ryan.williams93@aol.com>
2022-01-08 14:30:30 -0600

    added required packages needed for app

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,14 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (fileName, data) => {}
 
 // TODO: Create a function to initialize app
 function init() {}
