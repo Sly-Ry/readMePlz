e92f2d13f62feb842108659fb4596f742736b352
ryan <ryan.williams93@aol.com>
2022-01-08 15:20:18 -0600

    generated intitial questions for what info the user might want to include in their readme file

--- Develop/index.js
+++ Develop/index.js
@@ -5,11 +5,75 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = () => {
+    return inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter README.md title/Project title: (Required)',
+            validate: nameInput => {
+              if (nameInput) {
+                return true;
+              }
+              else {
+                console.log('Please enter your README.md title/Project Title');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'confirm',
+            name: 'confirmDescription',
+            message: 'Would you like to include a "Desciption" section?',
+            default: true
+        },
+        {
+            type: 'confirm',
+            name: 'confirmContentsTable',
+            message: 'Would you like to include a "Table of Contents" section?',
+            default: true
+        },
+        {
+            type: 'confirm',
+            name: 'confirmInstallation',
+            message: 'Would you like to include an "Installation" section?',
+            default: true
+        },
+        {
+            type: 'confirm',
+            name: 'confirmUsage',
+            message: 'Would you like to include a "Usage" section?',
+            default: true
+        },
+        {
+            type: 'confirm',
+            name: 'confirmLicense',
+            message: 'Would you like to add a license to your README.md?',
+            default: true
+        },
+        {
+            type: 'confirm',
+            name: 'confirmContributing',
+            message: 'Would you like to include a "Contributing" section?',
+            default: true
+        },
+        {
+            type: 'confirm',
+            name: 'confirmTesting',
+            message: 'Would you like to include a "Tests" section?',
+            default: true
+        },
+        {
+            type: 'confirm',
+            name: 'confirmQuestions',
+            message: 'Would you like to include a "Questions" section?',
+            default: true
+        }
+    ])
+};
 // TODO: Create a function to write README file
-const generateRM = () => 'Name: Ryan, Github: ryanhub';
-console.log(generateRM);
+
 
 const writeToFile = (fileName, data) => {}
 
@@ -18,3 +82,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+questions();
