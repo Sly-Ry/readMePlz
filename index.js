73dfff366566fdb954e7d5678ddc057eceea0e3a
ryan <ryan.williams93@aol.com>
2022-07-17 16:08:12 -0500

    refactor 'questions' for user input

--- index.js
+++ index.js
@@ -1,56 +1,76 @@
-// The packages needed for this application
+// packages
 const fs = require('fs');
 const inquirer = require('inquirer');
+const util = require('util');
+
+// modules
+const api = require('./utils/api');
 const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // An array of questions for user input
 const questions = [
     {
         type: 'input',
-        name: 'name',
-        message: "What's your name? (Required)",
+        message: 'What is your GitHub username:',
+        name: 'username',
+        default: 'Sly-Ry',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log('A valid GitHub username is required.');
+            }
+            return true;
+        }
     },
     {
         type: 'input',
-        name: 'title',
-        message: 'What is the title of your project? (Required)',
+        message: 'What is your GitHub repository title:',
+        name: 'repo',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log('A valid GitHub repo is required for a badge.');
+            }
+            return true;
+        }
     },
     {
         type: 'input',
+        message: 'Provide a short description of your project:',
         name: 'description',
-        message: 'Provide a short description of your project: (Required)',
+        default: 'Project Description',
+        validation: function (answer) {
+            if (answer.length < 1) {
+                return console.log('A valid project description is required.')
+            }
+            return true;
+        }
     },
     {
         type: 'input',
+        message: 'If applicable, describe the steps required to install your project for the Installation section:',
         name: 'installation',
-        message: 'Provide a step-by-step description of how to run your project:',
+        
     },
     {
         type: 'input',
+        message: 'Provide instructions and examples for use for the Usage section:',
         name: 'usage',
-        message: 'Provide instructions and examples for use:',
+    },
+    {
+        type: 'input',
+        message: 'If applicable, provide instructions on how other developers can contribute to you project:',
+        name: 'contribute'
+    },
+    {
+        type: 'input',
+        message: 'If applicable, provide any tests written for your application and provide examples on how to run them:',
+        name: 'tests'
     },
     {
         type: 'list',
-        name: 'license',
         message: 'Which license would you like to use?',
+        name: 'license',
         choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
     },
-    {
-        type: 'input',
-        name: 'credits',
-        message: 'Please add your credits: (if any)',
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: "What's your GitHub User Name? (Required)",
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: "What's your email address?",
-    }
 ];
 
 // A function to write README file
